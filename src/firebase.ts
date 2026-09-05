16ee963613497322b13e0d9c688172ba89a748a7
palomagcode-josh <qrjparnaso@tip.edu.ph>
2026-09-06 03:00:51 +0800

    Secure Firebase configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,14 @@ import { Auth, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8ad02BozK8L2PLBtnCCxSaPc0YpbP1PY",
-  authDomain: "code-quest-7191f.firebaseapp.com",
-  projectId: "code-quest-7191f",
-  storageBucket: "code-quest-7191f.firebasestorage.app",
-  messagingSenderId: "3533858849",
-  appId: "1:3533858849:web:840a7713234830730ddb60",
-  measurementId: "G-F6JQ8ZZ0J4"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length
@@ -17,8 +18,6 @@ const app = getApps().length
   : initializeApp(firebaseConfig);
 
 const auth: Auth = getAuth(app);
-
 const db = getFirestore(app);
 
 export { auth, db };
-
